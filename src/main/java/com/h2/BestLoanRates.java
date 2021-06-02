2ff2f4657305ef21892a83c7c72f93d1f2dd3ad3
Sudokue <80366215+Sudokue@users.noreply.github.com>
2021-06-02 12:03:28 -0400

    updatd

---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,26 @@
+package com.h2;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class BestLoanRates {
+
+    public static final Map<Integer, Float> bestRates = Map.of(
+            1, 5.50f, 2, 3.45f, 3, 2.67f
+    );
+
+
+    public static void main( String[] Args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Hello" + name);
+
+    }
+    public static float getRates(int loanTermInYears) {
+        if(bestRates.containsKey(loanTermInYears)) {
+            return bestRates.get(loanTermInYears);
+        }
+        return 0.0f;
+    }
+}
